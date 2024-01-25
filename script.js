30190b1ebd29d23f0774e3b177b7fc6db74715dc
Maurot11 <mauro.co.uk@hotmail.com>
2024-01-25 14:06:29 +0000

    clicking thumnails updates display image & img alt description in uppercase

--- script.js
+++ script.js
@@ -1,75 +1,119 @@
 const form = document.getElementById('form')
 const rawImages = document.getElementById('rawImages')
 const body = document.querySelector('body')
-const TTS = document.getElementById('altTxt')
+const TTS = document.getElementById('altTxtDiv')
 const thumbnails = document.getElementById('thumbnails')
 const next = document.getElementById('next')
 const previous = document.getElementById('previous')
 const txtDiv = document.getElementById('txtDiv')
+const nextImg = document.getElementById('nextImg')
+
 
 let images = []
 let imgIndex = 0
+let nextImgIndex = 1
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
     let imgQuery = e.target.input.value
     search(imgQuery)
-    console.log(imgQuery)
 })
 
 next.addEventListener('click', function() {
     console.log('NEXT')
+    if (images.length > 0) {
+        imgIndex++
+        nextImgIndex++
+        loadImgs(images[imgIndex])
+        loadNextImg(images[nextImgIndex])
+    } else if (images.length < 10) {
+        console.log('NOTHING NEXT')
+    }
 })
 
 previous.addEventListener('click', function() {
     console.log('Previous')
+    if (images.length > 1) {
+        imgIndex--
+        loadImgs(images[imgIndex])
+    } else if (images.length == 0) {
+        console.log('NOTING')
+    }
 })
 
 async function search(searchParam) {
-    let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchParam}?orientation=landscape&client_id=6rfnZF6E-D2i5eNGx326prxPJT2P1JP71C-YrJvYpMM`)
+    console.log(searchParam)
+    let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchParam}&orientation=landscape&client_id=6rfnZF6E-D2i5eNGx326prxPJT2P1JP71C-YrJvYpMM`)
     let data = await response.json()
     images = data.results
-    // console.log(images[imgIndex])
+    // nImg = data.results[imgIndex++]
+    
     loadImgs(images[imgIndex])
+
+    // DISPLAYS BUTTONS AND HIDES HEADER TEXT AFTER SEARCHING PHOTOS
     next.style.display = 'block'
     previous.style.display = 'block'
     txtDiv.style.display = 'none'
-    // let img = document.createElement('img')
-    // img.src = data.results[0].urls.raw
-    // rawImages.appendChild(img)
+    TTS.style.display = 'flex'
+    thumbnails.style.display = 'flex'
+    body.style.backgroundColor = 'black'
 }
 
+// DISPLAYS IMAGES TO THE SCREEN, THUMBSNAILS AND ASSIGNS ALT TEXT TO A CALLBACK FUNCTION
 function loadImgs(arrayImgs) {
     rawImages.innerHTML = ''
     thumbnails.innerHTML = ''
-    console.log(arrayImgs)
+    // console.log(arrayImgs)
 
     let imgTag = document.createElement('img')
 
+    // IMG TO DISPLAY
     imgTag.src = arrayImgs.urls.raw
     imgTag.alt = arrayImgs.alt_description
+    // console.log(imgTag)
+
+    display(imgTag)
     ttsAlt(arrayImgs.alt_description)
-    console.log(imgTag)
 
-    rawImages.appendChild(imgTag)
-    ttsAlt(arrayImgs.alt_description)
-
-
-    // apiImgs.forEach(img => {
-    //     let imgs = document.createElement('img')
-    //     imgs.src = img.urls.raw
-    //     rawImages.appendChild(imgs)
-    // })
     
+    // RENDERS ALL PHOTOS FROM FETCH AS THUMBNAILS AT THE TOP OF THE SCREEN
     images.forEach(img => {
         let imgs = document.createElement('img')
-        imgs.src = img.urls.raw
+        let imgRaw = document.createElement('img')
+
+        imgs.src = img.urls.thumb
+        imgs.alt = img.alt_description
+
+        imgRaw.src = img.urls.raw
 
         thumbnails.appendChild(imgs)
+
+        imgs.addEventListener('click', () => {
+            display(imgRaw)
+            ttsAlt(img.alt_description)
+        })
+
+        
     })
 }
 
+function loadNextImg(arrNextImg) {
 
+    let nextImgTag = document.createElement('img')
+    // LOADS NEXT IMG OFF SCREEN
+    nextImgTag.src = arrNextImg.urls.raw
+    console.log(nextImgTag)
+
+    nextImg.appendChild(nextImgTag)
+}
+
+function display(source) {
+    rawImages.innerHTML = ''
+
+    rawImages.appendChild(source)
+}
+
+// DISPLAYS ALT TEXT TO HTML
 function ttsAlt(altTxt) {
-    TTS.textContent = `Image description: ${altTxt}`
+    TTS.textContent = `Image description: ${altTxt}`.toUpperCase()
 }
